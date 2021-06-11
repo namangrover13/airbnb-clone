a3c78fbfb9b956b13fae5ed81a7bfa32192f474c
Vipul <44301794+vipulxd@users.noreply.github.com>
2021-06-11 23:59:01 +0530

    Added Covid-19 Response  Tab
    Minor changes

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -3,34 +3,38 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../../styles/navbar.css";
 function index() {
   return (
-    <div className="nav-outer">
-      <div className="nav-child navchild1 ">
-        <i className="fab fa-airbnb fa-4x"></i>{" "}
-      </div>
-      <div className="nav-child navchild2 item-center">
-        <p>Place to stay</p>
-      </div>
-      <div className="nav-child navchild3 item-center">
-        <p>Experiences</p>
-      </div>
-      <div className="nav-child navchild4 item-center">
-        <p>Online experiences</p>
-      </div>
-      <div className="nav-child  item-center navchild5">
-        <p>Become a host</p>
-      </div>
-      <div className="nav-child last-two navchild6">
-        {" "}
-        <p>
+    <div>
+      {" "}
+      <div className="top_text">Get the latest on our COVID-19 response</div>
+      <div className="nav-outer">
+        <div className="nav-child navchild1 ">
+          <i className="fab fa-airbnb fa-4x"></i>{" "}
+        </div>
+        <div className="nav-child navchild2 item-center">
+          <p>Place to stay</p>
+        </div>
+        <div className="nav-child navchild3 item-center">
+          <p>Experiences</p>
+        </div>
+        <div className="nav-child navchild4 item-center">
+          <p>Online experiences</p>
+        </div>
+        <div className="nav-child  item-center navchild5">
+          <p>Become a host</p>
+        </div>
+        <div className="nav-child last-two navchild6">
           {" "}
-          <i class="far fa-globe fa-1x"></i>
-        </p>
-      </div>
-      <div className="nav-child last-two navchild7">
-        <p>
-          {" "}
-          <i class="far fa-user fa-1x"></i>
-        </p>
+          <p>
+            {" "}
+            <i class="far fa-globe fa-1x"></i>
+          </p>
+        </div>
+        <div className="nav-child last-two navchild7">
+          <p>
+            {" "}
+            <i class="far fa-user fa-1x"></i>
+          </p>
+        </div>
       </div>
     </div>
   );
