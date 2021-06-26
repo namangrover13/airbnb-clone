2f11470feb992dc2afdcb0bb25e108e159bc74ca
Vipul <44301794+vipulxd@users.noreply.github.com>
2021-06-26 13:07:49 +0530

    deploy

--- src/components/anywhere/Anywhere.js
+++ src/components/anywhere/Anywhere.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import Varieties from './Varieties';
-import '../../styles/anywhere.css'
-import outdoor from '../../images/outdoor.jpg'
-import entire from '../../images/entire.jpg'
-import pet from '../../images/pet.jpg'
-import unique from '../../images/unique.jpg'
-const Anywhere = (props) => {
-    return ( 
-        <div className="anywhere">
-            <h1>Live Anywhere!</h1>
-            <div className="anywhere-varieties">
-                {props.Variety.map((v) => (
-                    <Varieties imgName={v.imgName} tagline={v.tagline}/>
-                ))}
-            </div>
-        </div>
-     );
-}
-Anywhere.defaultProps={
-    Variety : [
-        { imgName : outdoor, tagline : 'Outdoor Getaway' },
-        { imgName : entire, tagline : 'Entire Homes' },
-        { imgName : pet, tagline : 'Pets Allowed' },
-        { imgName : unique, tagline : 'Unique Stays', }
-    ]
-}
- 
+import React from "react";
+import Varieties from "./Varieties";
+import "../../styles/anywhere.css";
+import outdoor from "../../images/outdoor.jpg";
+import entire from "../../images/entire.jpg";
+import pet from "../../images/pet.jpg";
+import unique from "../../images/unique.jpg";
+const Anywhere = props => {
+  return (
+    <div className="anywhere">
+      <h1>Live Anywhere!</h1>
+      <div className="anywhere-varieties">
+        {props.Variety.map((v, key) => (
+          <Varieties imgName={v.imgName} key={key} tagline={v.tagline} />
+        ))}
+      </div>
+    </div>
+  );
+};
+Anywhere.defaultProps = {
+  Variety: [
+    {imgName: outdoor, tagline: "Outdoor Getaway"},
+    {imgName: entire, tagline: "Entire Homes"},
+    {imgName: pet, tagline: "Pets Allowed"},
+    {imgName: unique, tagline: "Unique Stays"},
+  ],
+};
+
 export default Anywhere;
--- src/components/anywhere/Varieties.js
+++ src/components/anywhere/Varieties.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import '../../styles/varieties.css'
+import React from "react";
+import "../../styles/varieties.css";
 
-const Varieties = (props) => {
+const Varieties = props => {
+  return (
+    <div className="varieties">
+      <img className="varieties-image" src={props.imgName} />
+      <p>{props.tagline}</p>
+    </div>
+  );
+};
 
-    return ( 
-        <div className="varieties">
-            <img className="varieties-image" src={props.imgName}/>
-            <p>{props.tagline}</p>
-        </div>
-     );
-}
- 
 export default Varieties;
